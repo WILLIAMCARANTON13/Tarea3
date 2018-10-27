b10eebb3ae08cbbf18aaa0f7784873c7b611a4dc
Santiago <wg.caranton10@uniandes.edu.co>
2018-10-27 18:31:52 -0500

    Encontradas frecuencias de los picos

--- Fourier.py
+++ Fourier.py
@@ -27,5 +27,22 @@ plt.savefig("CarantonWilliam_signal.pdf")
 F=Fourier(Ydata)
 freq=np.fft.fftfreq(len(Ydata),dx)
 
-plt.plot(freq,np.sqrt(np.real(F)**2+np.imag(F)))
+FR=np.sqrt(np.real(F)**2+np.imag(F)**2)
+
+plt.plot(freq,FR)
 plt.savefig("CarantonWilliam_TF.pdf")
+
+FR2=FR
+freq_max=[]
+F_max=[]
+
+for i in range(6):
+	index=list(FR).index(max(FR2))
+	if freq[index]>0:
+		F_max.append(FR[index])
+		freq_max.append(freq[index])
+	FR2[index]=0
+	
+print("Las frecuencias donde están los picos son:",freq_max)
+	
+

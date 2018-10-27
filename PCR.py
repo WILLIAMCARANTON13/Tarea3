c8e047bf259dc35ac4a3152bdd691eddb485ac14
Santiago <wg.caranton10@uniandes.edu.co>
2018-10-27 17:45:27 -0500

    Corrección de matriz de covarianza punto 2

--- PCR.py
+++ PCR.py
@@ -33,9 +33,13 @@ def cov_matrix(matriz):
 	return cov/(N-1)
 
 
-covarianza=cov_matrix(np.transpose(valores[:10]))
+covarianza=cov_matrix(valores[:50])
+
 
 a_val,a_vec=np.linalg.eig(covarianza)
+
+#a_vec=np.real(a_vec)
+#a_val=np.real(a_val)
 
 for i in range(len(a_val)):
 	print("Autovector: ",a_vec[i],"   autovalor correspondiente: ",a_val[i])
@@ -55,7 +59,7 @@ PC2=a_vec[indice_mayor_a_val_2]
 
 print("\n Las direcciones a las que los datos muestran mayor tendencia son los autovalores:\n\n",PC1,"\n\n",PC2,"\n\n","elegidos basándose en sus autovalores.")
 
-
+#print(len(PC1))
 
 
 
